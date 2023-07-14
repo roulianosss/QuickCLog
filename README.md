eaf3172dace884832cef68270d7393a23dce4a5f
Julien <julien.ortscheid@gmail.com>
2023-07-14 09:04:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Variable Log Extension for VS Code
+
+This extension allows you to quickly insert `console.log` statements before or after a selected variable in your code.
+
+## Features
+
+- Insert `console.log` before a variable: This command inserts a `console.log` statement before the selected variable in the active text editor.
+
+- Insert `console.log` after a variable: This command inserts a `console.log` statement after the selected variable in the active text editor.
+
+## Requirements
+
+- Visual Studio Code 1.50.0 or later
+
+## Installation
+
+1. Launch Visual Studio Code.
+2. Go to the Extensions view (Ctrl+Shift+X).
+3. Search for "Variable Log" and click Install.
+4. Restart Visual Studio Code (if required).
+
+## Usage
+
+1. Open a JavaScript or TypeScript file in the editor.
+2. Select the variable you want to log.
+3. Code Actions on the selected variable or use the command palette (Ctrl+Shift+P).
+4. Choose either "Insert console.log before line" or "Insert console.log after line" from the available commands.
+5. A `console.log` statement will be inserted at the appropriate position.
+
+## Known Issues
+
+- No known issues at the moment. If you encounter any problems, please [report them](https://github.com/your-repo-url/issues).
+
+## Contributing
+
+Contributions are welcome! Please follow these steps to contribute:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push your changes to your fork.
+5. Submit a pull request to the main repository.
+
+## License
+
+This extension is released under the [MIT License](https://opensource.org/licenses/MIT). See the [LICENSE](LICENSE) file for more details.
+
+## Contact
+
+For any questions or suggestions, feel free to reach out to the project maintainer:
+
+
---
+++ src/extension.ts
@@ -0,0 +1,64 @@
+import * as vscode from 'vscode';
+
+function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('extension.logBeforeLine', () => {
+    insertLogStatement(true);
+  });
+
+  let disposable2 = vscode.commands.registerCommand('extension.logAfterLine', () => {
+    insertLogStatement(false);
+  });
+
+  vscode.languages.registerCodeActionsProvider('*', {
+    provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext) {
+      const diagnostic = new vscode.Diagnostic(
+        range,
+        'Insert console.log',
+        vscode.DiagnosticSeverity.Hint
+      );
+
+      const beforeLineAction = new vscode.CodeAction('Insert console.log before line', vscode.CodeActionKind.QuickFix);
+      beforeLineAction.command = { command: 'extension.logBeforeLine', title: 'Insert Console.log Before Line' };
+      beforeLineAction.diagnostics = [diagnostic];
+
+      const afterLineAction = new vscode.CodeAction('Insert console.log after line', vscode.CodeActionKind.QuickFix);
+      afterLineAction.command = { command: 'extension.logAfterLine', title: 'Insert Console.log After Line' };
+      afterLineAction.diagnostics = [diagnostic];
+      console.log('afterLineAction', afterLineAction);
+
+      console.log('beforeLineAction', beforeLineAction);
+      return [beforeLineAction, afterLineAction];
+    }
+  });
+
+  context.subscriptions.push(disposable, disposable2);
+}
+
+function insertLogStatement(beforeLine: boolean) {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const document = editor.document;
+    const selection = editor.selection;
+    const position = selection.active;
+    const line = position.line;
+    const text = document.getText(selection) ? document.getText(selection) : document.getText(document.getWordRangeAtPosition(position));
+    const lineText = document.lineAt(line).text;
+    const indentation = lineText.match(/^\s*/)?.[0];
+
+    const logStatement = `${indentation}console.log('${text}', ${text});\n`;
+
+    if (beforeLine) {
+      const newLine = new vscode.Position(line, 0); 
+      editor.edit((editBuilder: vscode.TextEditorEdit) => {
+        editBuilder.insert(newLine, logStatement);
+      });
+    } else {
+      const newLine = new vscode.Position(line + 1, 0); 
+      editor.edit((editBuilder: vscode.TextEditorEdit) => {
+        editBuilder.insert(newLine, logStatement);
+      });
+    }
+  }
+}
+
+export { activate };
